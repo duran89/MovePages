a0545c6ee60ce1251c2205a84ca1a56a7926ca3c
duran89 <leral3@naver.com>
2024-01-14 20:01:36 +0900

    화면전환

--- MovePages/DetailViewController.swift
+++ MovePages/DetailViewController.swift
@@ -8,22 +8,71 @@
 import UIKit
 
 class DetailViewController: UIViewController {
+    
+    // MARK: 메인 페이지에서 데이터 받을 변수
+    // MARK: 데이터를 주고 받을 때는 변수를 통할 것
+    var mainMessage: String?
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    
+    // 디테일 라벨 설정
+    private var detailLabel: UILabel = {
+        var label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = ""
+        label.font = .systemFont(ofSize: 22, weight: .bold)
+        label.textColor = .green
+        return label
+    }()
+    
+    // 버튼 구현
+    private var BackToMainButton: UIButton = {
+        var button = UIButton(type: .system)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Back", for: .normal)
+        button.setTitleColor(UIColor.label, for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 22, weight: .bold)
+        button.backgroundColor = UIColor.green
+        button.layer.masksToBounds = true
+        button.layer.cornerRadius = 10
+        
+        // 버튼 함수 구현
+        button.addTarget(Any?.self, action: #selector(backToMain), for: .touchUpInside)
+        return button
+    }()
+    
+    // 버튼 함수
+    @objc func backToMain() {
+        dismiss(animated: true)
     }
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        view.addSubview(detailLabel)
+        view.addSubview(BackToMainButton)
+        
+        detailLabel.text = mainMessage
+        configureConstraints()
     }
-    */
-
+    
+    // 제약 조건
+    func configureConstraints() {
+        
+        let detailLabelContraints = [
+            detailLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            detailLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ]
+        
+        let moveToDetailButtonConstraints = [
+            BackToMainButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            BackToMainButton.topAnchor.constraint(equalTo: detailLabel.bottomAnchor, constant: 20),
+            BackToMainButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
+            BackToMainButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20)
+        ]
+        
+        
+        NSLayoutConstraint.activate(detailLabelContraints)
+        NSLayoutConstraint.activate(moveToDetailButtonConstraints)
+    }
 }
--- MovePages/ViewController.swift
+++ MovePages/ViewController.swift
@@ -8,12 +8,75 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // 라벨 구현
+    private var mainLabel: UILabel = {
+        var label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "메인 화면"
+        label.font = .systemFont(ofSize: 22, weight: .bold)
+        label.textColor = .green
+        return label
+    }()
+    
+    
+    
+    // 버튼 구현
+    private var moveToDetailButton: UIButton = {
+        var button = UIButton(type: .system)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("NEXT", for: .normal)
+        button.setTitleColor(UIColor.label, for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 22, weight: .bold)
+        button.backgroundColor = UIColor.green
+        button.layer.masksToBounds = true
+        button.layer.cornerRadius = 10
+        
+        // 다음 페이지로 이동 함수
+        button.addTarget(Any?.self, action: #selector(moveToDetail), for: .touchUpInside)
+        
+        return button
+    }()
+    
+    // 버튼 함수 구현
+    @objc func moveToDetail() {
+        let detailVC = DetailViewController()
+        
+        detailVC.mainMessage = "성공"
+        
+        detailVC.modalPresentationStyle = .fullScreen
+        present(detailVC, animated: true, completion: nil)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .red
+        
+        view.addSubview(mainLabel)
+        view.addSubview(moveToDetailButton)
+        
+        configureConstraints()
     }
-
-
+    
+    // 제약 조건
+    func configureConstraints() {
+        
+        let mainLabelContraints = [
+            mainLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            mainLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ]
+        
+        let moveToDetailButtonConstraints = [
+            moveToDetailButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            moveToDetailButton.topAnchor.constraint(equalTo: mainLabel.bottomAnchor, constant: 20),
+            moveToDetailButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
+            moveToDetailButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20)
+        ]
+        
+        
+        NSLayoutConstraint.activate(mainLabelContraints)
+        NSLayoutConstraint.activate(moveToDetailButtonConstraints)
+    }
 }
 
